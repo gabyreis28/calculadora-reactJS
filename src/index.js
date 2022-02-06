6875c466436cb661e6cfb759213ff5a397ff547e
gabyreis28 <gabriela.dias.97@hotmail.com>
2022-02-06 00:45:24 -0300

    Componente Calculator e implementacao logica

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+import Button from '../components/Button';
+import Display from '../components/Display';
+
+const initalState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+};
+
+export default class Calculator extends Component {
+  state = { ...initalState };
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+
+  clearMemory() {
+    this.setState({ ...initalState });
+  }
+
+  setOperation(operation) {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    }
+  }
+
+  addDigit(n) {
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n != '.') {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+
+      console.log(values);
+    }
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Display value={this.state.displayValue} />
+        <Button label={'AC'} click={this.clearMemory} triple />
+        <Button label={'/'} click={this.setOperation} operation />
+        <Button label={'7'} click={this.addDigit} />
+        <Button label={'8'} click={this.addDigit} />
+        <Button label={'9'} click={this.addDigit} />
+        <Button label={'*'} click={this.setOperation} operation />
+        <Button label={'4'} click={this.addDigit} />
+        <Button label={'5'} click={this.addDigit} />
+        <Button label={'6'} click={this.addDigit} />
+        <Button label={'-'} click={this.setOperation} operation />
+        <Button label={'1'} click={this.addDigit} />
+        <Button label={'2'} click={this.addDigit} />
+        <Button label={'3'} click={this.addDigit} />
+        <Button label={'+'} click={this.setOperation} operation />
+        <Button label={'0'} click={this.addDigit} double />
+        <Button label={'.'} click={this.addDigit} />
+        <Button label={'='} click={this.setOperation} operation />
+      </div>
+    );
+  }
+}
