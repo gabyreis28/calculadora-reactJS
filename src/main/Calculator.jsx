c009fd25ff844c792bd7da8cf975c7b86efb2033
gabyreis28 <gabriela.dias.97@hotmail.com>
2022-02-06 19:53:06 -0300

    Implementação de logica

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,6 +27,26 @@ export default class Calculator extends Component {
   setOperation(operation) {
     if (this.state.current === 0) {
       this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const currentOperatio = this.state.operation;
+      const values = [...this.state.values];
+
+      try {
+        values[0] = eval(`${values[0]} ${currentOperatio[0]} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      });
     }
   }
 
